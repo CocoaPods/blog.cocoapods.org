b1469f71eaee950472ebd9710229339236dea20e
Keith Smiley <keithbsmiley@gmail.com>
2013-08-15 14:28:17 -0400

    Clone dependencies in rakefile

--- rakefile.rb
+++ rakefile.rb
@@ -2,6 +2,9 @@
 
 desc 'Build the site for deployment'
 task :build do
+  puts "Cloning submodules..."
+  puts %x[git submodule update --init --recursive]
+
   puts "Compiling sass..."
   puts %x[sass _sass/main.scss css/main.css --style=compressed]
 
