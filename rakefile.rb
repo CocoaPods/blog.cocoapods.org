15f15b0b86df3e1ccc69248e55696e182eee6e44
Fabio Pelosin <fabiopelosin@gmail.com>
2013-10-09 21:31:50 +0200

    Don't use a submoduel for the checkout of the gh-pages branch

--- rakefile.rb
+++ rakefile.rb
@@ -2,8 +2,17 @@
 
 desc 'Initial setup'
 task :init do
+  FileUtils.rm_rf '_gh-pages'
+  puts "Cloning gh-pages branch..."
+  puts %x[git clone git@github.com:CocoaPods/blog.cocoapods.org.git _gh-pages]
+  Dir.chdir('_gh-pages') do
+    puts %x[git checkout gh-pages]
+  end
+
   puts "Cloning submodules..."
   puts %x[git submodule update --init --recursive]
+
+  puts "Installing Bundle..."
   puts %x[bundle install]
 end
 
@@ -12,17 +21,18 @@ task :deploy do
 
   puts "Building site."
   puts %x[jekyll build -d _gh-pages]
-  
+
   Dir.chdir("_gh-pages") do
     puts "Pulling changes from server."
+    puts %x[git checkout gh-pages]
     puts %x[git pull origin gh-pages]
 
     puts "Creating a commit for the deploy."
-    
+
     puts %x[git ls-files --deleted -z | xargs -0 git rm;]
     puts %x[git add .]
     puts %x[git commit -m "Deploy"]
-    
+
     puts "Pushing to github."
     puts %x[git push ]
   end
